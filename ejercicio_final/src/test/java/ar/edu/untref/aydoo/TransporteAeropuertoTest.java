f1f7ced1353c8b7b09801dcb2dd2293f635a2fcb
emiliano1987 <emilianoveppo@gmail.com>
2016-12-22 01:50:29 -0300

    Se agrega test para validar tipo de transporte y se modifican nombres de test

--- ejercicio_final/src/test/java/ar/edu/untref/aydoo/TransporteAeropuertoTest.java
+++ ejercicio_final/src/test/java/ar/edu/untref/aydoo/TransporteAeropuertoTest.java
@@ -4,38 +4,44 @@ import org.junit.Test;
 public class TransporteAeropuertoTest {
 
 	@Test
-	public void TransporteAeropuertoCobra0SiEsTrenInterno(){
+	public void TransporteCobra0SiEsTrenInterno(){
 		TransporteAeropuerto transporteAeroportuario = new TransporteAeropuerto("Tren Interno");
 
 		Assert.assertTrue(transporteAeroportuario.cobrarPasaje("Terminal A", "Terminal B", 2) == 0);
 	}
 
 	@Test
-	public void TransporteAeropuertoCobra0SiEsMinibusEnTerminalesIguales(){
+	public void TransporteCobra0SiEsMinibusEnTerminalesIguales(){
 		TransporteAeropuerto transporteMinibus = new TransporteAeropuerto("Minibus");
 
 		Assert.assertTrue(transporteMinibus.cobrarPasaje("Terminal A", "Terminal A", 3) == 0);
 	}
 
 	@Test
-	public void TransporteAeropuertoCobra5SiEsMinibusEnDiferentesTerminales(){
+	public void TransporteCobra5SiEsMinibusEnDiferentesTerminales(){
 		TransporteAeropuerto transporteMinibus = new TransporteAeropuerto("Minibus");
 
 		Assert.assertTrue(transporteMinibus.cobrarPasaje("Terminal A", "Terminal B", 2) == 5);
 	}
 
 	@Test
-	public void TransporteAeropuertoCobra10SiEsMinibusEnTerminalesAYC(){
+	public void TransporteCobra10SiEsMinibusEnTerminalesAYC(){
 		TransporteAeropuerto transporteMinibus = new TransporteAeropuerto("Minibus");
 
 		Assert.assertTrue(transporteMinibus.cobrarPasaje("Terminal A", "Terminal C", 2) == 10);
 	}
 
 	@Test
-	public void TransporteAeropuertoCobra10SiEsCochePrivadoEnTerminalesIguales(){
-		TransporteAeropuerto transporteMinibus = new TransporteAeropuerto("Minibus");
+	public void TransporteCobra121SiEsCochePrivadoEnTerminalesIgualesCon10Pasajeros(){
+		TransporteAeropuerto transporteCochePrivado = new TransporteAeropuerto("Coche Privado");
 
-		Assert.assertTrue(transporteMinibus.cobrarPasaje("Terminal A", "Terminal A", 2) == 0);
+		Assert.assertTrue(transporteCochePrivado.cobrarPasaje("Terminal A", "Terminal A", 10) == 121);
 	}
 
+	@Test
+	public void TransporteCobra121SiEsCochePrivadoEnTerminalesDiferentesCon5Pasajeros(){
+		TransporteAeropuerto transporteCochePrivado = new TransporteAeropuerto("Coche Privado");
+		
+		Assert.assertTrue(transporteCochePrivado.cobrarPasaje("Terminal A", "Terminal B", 5) == 121);
+	}
 }
