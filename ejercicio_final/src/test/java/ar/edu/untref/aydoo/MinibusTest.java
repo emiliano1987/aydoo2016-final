0cfab0d98ed6e853b4562c406aea26501464bf21
emiliano1987 <emilianoveppo@gmail.com>
2016-12-23 18:15:08 -0300

    Se definen test para clase Minibus

--- ejercicio_final/src/test/java/ar/edu/untref/aydoo/MinibusTest.java
+++ ejercicio_final/src/test/java/ar/edu/untref/aydoo/MinibusTest.java
@@ -4,9 +4,24 @@ import org.junit.Test;
 public class MinibusTest {
 
 	@Test
-	public void TransporteCobra0SiEsMinibusEnTerminalesOrigenYDestinoA(){
+	public void MinibusCobra0SiEsMinibusEnTerminalesOrigenYDestinoA(){
 		Transporte transporteMinibus = new Minibus();
 
 		Assert.assertTrue(transporteMinibus.cobrarPasajeSegunTransporte("Terminal A", "Terminal A", 3) == 0);
 	}
+	
+	@Test
+	public void MinibusCobra0SiEsTerminalesOrigenYDestinoB(){
+		Transporte transporteMinibus = new Minibus();
+
+		Assert.assertTrue(transporteMinibus.cobrarPasajeSegunTransporte("Terminal B", "Terminal B", 3) == 0);
+	}
+	
+	@Test
+	public void MinibusCobra0SiEsMinibusEnTerminalesOrigenYDestinoC(){
+		Transporte transporteMinibus = new Minibus();
+
+		Assert.assertTrue(transporteMinibus.cobrarPasajeSegunTransporte("Terminal C", "Terminal C", 3) == 0);
+	}
+
 }
